3ee4591da47515e374cce0df1b595710554eef73
Yuvati Mankar <yuvatimankar06@gmail.com>
2022-09-16 13:50:37 +0530

    portflio java project

---
+++ Authorization/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+drop table if exists userrecord;
+create table userrecord(user_id int primary key ,user_name varchar(30),user_password varchar(20));
+
+
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (101,'blessy','1234');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (102,'pratheepa','6287');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (103,'madhav','8822');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (104,'saif','7612');
